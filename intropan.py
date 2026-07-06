c2889c8877c1fc6625fb9dc8c024338db9d620b3
João Vitor Penha <joao.penha.martins47@gmail.com>
2026-07-06 15:36:16 -0300

    pandas ep 1

--- intropan.py
+++ intropan.py
@@ -1,6 +1,25 @@
 import pandas as pd
 
-valores = pd.Series(["1.689,52", "- 18,50", "- 25,00", "89,90"])
+#Leitura
+df_vendas = pd.read_csv("vendas_tech.csv", encoding='utf-8', sep=',', low_memory=False)
+df_loja = pd.read_excel("gerentes_lojas.xlsx")
 
-print(valores)
-print(valores.dtype)
+
+#Ler os 5 primeiros
+print(df_vendas.head())
+
+#Ler os 5 ultimos, pode-se colocar valores dentro do parametro para dizer quantos serao exibidos
+print(df_vendas.tail())
+
+#Pegar 5 amostras
+print(df_vendas.sample())
+
+#Saber o formato do teu arquivo (shape)
+print(f'shape: {df_vendas.shape}')
+
+#Saber as colunas
+print(df_vendas.columns)
+
+#Para limpeza de dados, saber as info e para dados estatisticos, podem ser uteis o info e o describe respectivamente
+print(df_vendas.info())
+print(df_vendas.describe())
